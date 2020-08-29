6e8502d0c8734ecc4e8913c50c7311879de20525
Denis Griza <grizadenis@icloud.com>
2020-08-29 12:06:41 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Денис Гриза on 29.08.2020.
+//  Copyright © 2020 Denis Griza. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+    
+    
+    
+}
